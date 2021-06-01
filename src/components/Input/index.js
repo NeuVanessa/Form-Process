c14b74d560de5356f84894f0e83dd06c6554a502
Vanessa Lopes <63751937+NeuVanessa@users.noreply.github.com>
2021-05-31 21:38:49 -0300

    add input

---
+++ src/components/Input/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {InputExemple} from './styles';
+
+function Input(props) {
+    return  <InputExemple placeholder={props.placeholder}/>;
+}
+
+export default Input;
---
+++ src/components/Input/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const InputExemple = styled.input`
+  display: flex;
+  width: 30px;
+  height: 30px;
+  justify-content: center;
+  align-items: center;
+  color: red;
+  border-radius: 8px;
+  border: none;
+`;
